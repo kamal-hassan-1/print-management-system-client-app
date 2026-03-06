08316e20f5eda12dbf67f5b463936104c2873d62
Kamal Hassan <scienceknowledge191andme@gmail.com>
2026-03-06 20:31:09 +0500

    changed slug, title and updated dependencies

--- app.config.js
+++ app.config.js
@@ -1,62 +1,62 @@
 import "dotenv/config";
 export default {
-  "expo": {
-    "name": "ClickPrint",
-    "slug": "ClickPrint",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "ClickPrint",
-    "userInterfaceStyle": "automatic",
-    "splash": {
-      "image": "./assets/images/icon.png",
-      "resizeMode": "cover",
-      "backgroundColor": "#000000"
-    },
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true
-    },
-    "android": {
-      "adaptiveIcon": {
-        "backgroundColor": "#E6F4FE",
-        "foregroundImage": "./assets/images/icon.png",
-        "backgroundImage": "./assets/images/icon.png",
-        "monochromeImage": "./assets/images/icon.png"
-      },
-      "edgeToEdgeEnabled": true,
-      "predictiveBackGestureEnabled": false,
-      "package": "com.kamalhassan.printmanagementsystemclientapp"
-    },
-    "web": {
-      "output": "static",
-      "favicon": "./assets/images/icon.png"
-    },
-    "plugins": [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          "image": "./assets/images/icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#ffffff",
-          "dark": {
-            "backgroundColor": "#000000"
-          }
-        }
-      ]
-    ],
-    "experiments": {
-      "typedRoutes": true,
-      "reactCompiler": true
-    },
-    "extra": {
-      "router": {},
-      "eas": {
-        "projectId": "8f036921-80b8-45ba-9cfa-120c5411b3a6"
-      },
-      "apiBaseUrl": process.env.API_BASE_URL,
-    }
-  }
-}
+	expo: {
+		name: "Click Print",
+		slug: "Click Print",
+		version: "1.0.0",
+		orientation: "portrait",
+		icon: "./assets/images/icon.png",
+		scheme: "Click Print",
+		userInterfaceStyle: "automatic",
+		splash: {
+			image: "./assets/images/icon.png",
+			resizeMode: "cover",
+			backgroundColor: "#000000",
+		},
+		newArchEnabled: true,
+		ios: {
+			supportsTablet: true,
+		},
+		android: {
+			adaptiveIcon: {
+				backgroundColor: "#E6F4FE",
+				foregroundImage: "./assets/images/icon.png",
+				backgroundImage: "./assets/images/icon.png",
+				monochromeImage: "./assets/images/icon.png",
+			},
+			edgeToEdgeEnabled: true,
+			predictiveBackGestureEnabled: false,
+			package: "com.kamalhassan.printmanagementsystemclientapp",
+		},
+		web: {
+			output: "static",
+			favicon: "./assets/images/icon.png",
+		},
+		plugins: [
+			"expo-router",
+			[
+				"expo-splash-screen",
+				{
+					image: "./assets/images/icon.png",
+					imageWidth: 200,
+					resizeMode: "contain",
+					backgroundColor: "#ffffff",
+					dark: {
+						backgroundColor: "#000000",
+					},
+				},
+			],
+		],
+		experiments: {
+			typedRoutes: true,
+			reactCompiler: true,
+		},
+		extra: {
+			router: {},
+			eas: {
+				projectId: "8f036921-80b8-45ba-9cfa-120c5411b3a6",
+			},
+			apiBaseUrl: process.env.API_BASE_URL,
+		},
+	},
+};
